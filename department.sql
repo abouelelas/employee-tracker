c35e49435c4d9284346f16f681898e0b26682a3f
Sarah Abouelela <abouelelas@gmail.com>
2020-08-17 23:17:10 -0400

    added necesary files

---
+++ department.sql
@@ -0,0 +1,4 @@
+-- * **department**:
+
+--   * **id** - INT PRIMARY KEY
+--   * **name** - VARCHAR(30) to hold department name
---
+++ role.sql
@@ -0,0 +1,6 @@
+--  **role**:
+
+--   * **id** - INT PRIMARY KEY
+--   * **title** -  VARCHAR(30) to hold role title
+--   * **salary** -  DECIMAL to hold role salary
+--   * **department_id** -  INT to hold reference to department role belongs to
