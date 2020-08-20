7dff96d82ee3309ea6eb6e26535b8c0405efe5ca
Sarah Abouelela <abouelelas@gmail.com>
2020-08-20 14:41:49 -0400

    functions to add and view

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const inquirer = require('inquirer');
 const mysql = require('mysql');
+const Table = require('cli-table3');
 
 
-var connection = mysql.createConnection({
+const connection = mysql.createConnection({
     host: "localhost",
 
     // Your port; if not 3306
@@ -13,14 +14,14 @@ var connection = mysql.createConnection({
 
     // Your password
     password: "rootpassword",
-    database: "employees_db"
+    database: "employee_db"
 });
 connection.connect(function (err) {
     if (err) throw err;
-    start();
+    startQuestions();
 });
 
-function start() {
+function startQuestions() {
     inquirer
         .prompt({
             name: "choice",
@@ -44,6 +45,9 @@ function start() {
                 case "View all employees":
                     viewEmployees();
                     break;
+                case "View a department":
+                    viewDepartments();
+                    break;
                 case "Add a department":
                     addDepartment();
                     break;
@@ -60,11 +64,49 @@ function start() {
                     connection.end();
                     break;
                 default:
-                    start();
+                    startQuestions();
             }
         });
 }
+function viewEmployees() {
+    connection.query("SELECT * FROM employee", function (err, data) {
+        console.table(data);
+       startQuestions();
+    })
+}
+    function viewDepartments() {
+        connection.query("SELECT * FROM department", function (err, data) {
+            console.table(data);
+            startQuestions();
+        });
+    };
 
-
-
-
+function addEmployee(){
+        inquirer.prompt([{
+                type: "input",
+                name: "firstName",
+                message: "What is the employees first name?"
+            },
+            {
+                type: "input",
+                name: "lastName",
+                message: "What is the employees last name?"
+            },
+            {
+                type: "number",
+                name: "roleId",
+                message: "What is the employees role ID"
+            },
+            {
+                type: "number",
+                name: "managerId",
+                message: "What is the employees manager's ID?"
+            }
+        ]).then(function(res) {
+            connection.query('INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)', [res.firstName, res.lastName, res.roleId, res.managerId], function(err, data) {
+                if (err) throw err;
+                console.table("Successfully Inserted");
+                askQuestions();
+            })
+        })
+    }
