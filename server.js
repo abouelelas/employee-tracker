6b65aadea2a1b1bbfca7f08c4b15b8d64acdfe66
Sarah Abouelela <abouelelas@gmail.com>
2020-08-22 15:27:23 -0400

    fixed add employee function with manager id

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const inquirer = require('inquirer');
 const mysql = require('mysql');
-const Table = require('cli-table3');
-
+// const Table = require('cli-table3');
+const cTable = require('console.table');
 
 const connection = mysql.createConnection({
     host: "localhost",
 
-    // Your port; if not 3306
-    port: 3306,
+    // Your port
+    // port: 3306,
 
     // Your username
     user: "root",
@@ -15,9 +15,11 @@ const connection = mysql.createConnection({
     // Your password
     password: "rootpassword",
     database: "employee_db"
+
 });
 connection.connect(function (err) {
     if (err) throw err;
+    console.log("Connected to");
     startQuestions();
 });
 
@@ -39,13 +41,13 @@ function startQuestions() {
             ]
         }).then(answer => {
             switch (answer.choice) {
-                case "View all roles?":
+                case "View all roles":
                     viewRoles();
                     break;
                 case "View all employees":
                     viewEmployees();
                     break;
-                case "View a department":
+                case "View all departments":
                     viewDepartments();
                     break;
                 case "Add a department":
@@ -60,54 +62,85 @@ function startQuestions() {
                 case "Update employee role":
                     updateEmployee();
                     break;
-                case "Exit":
+                default:
                     connection.end();
                     break;
-                default:
-                    startQuestions();
+                // default:
+                //     startQuestions();
             }
         });
 }
 function viewEmployees() {
     connection.query("SELECT * FROM employee", function (err, data) {
+        console.log("\n");
         console.table(data);
-       startQuestions();
+        console.log("\n");
+        startQuestions();
     })
 }
-    function viewDepartments() {
-        connection.query("SELECT * FROM department", function (err, data) {
-            console.table(data);
-            console.log("\n");
-            startQuestions();
-        });
-    };
+function viewDepartments() {
+    connection.query("SELECT * FROM department", function (err, data) {
+        if (err) throw err;
+        console.log("\n");
+        console.table(data);
+        console.log("\n");
+        startQuestions();
+    });
+}
+function viewRoles() {
+    connection.query("SELECT * FROM role", function (err, data) {
+        if (err) throw err;
+        console.log("\n");
+        console.table(data);
+        console.log("\n");
+        startQuestions();
+    });
+}
 
-function addEmployee(){
-        inquirer.prompt([{
-                type: "input",
-                name: "firstName",
-                message: "What is the employees first name?"
-            },
-            {
-                type: "input",
-                name: "lastName",
-                message: "What is the employees last name?"
-            },
-            {
-                type: "number",
-                name: "roleId",
-                message: "What is the employees role ID"
-            },
-            {
-                type: "number",
-                name: "managerId",
-                message: "What is the employees manager's ID?"
+
+function addEmployee() {
+    connection.query("SELECT * FROM employee", (err, results)=>{
+        if (err)
+        throw(err);
+            // console.log(results);
+        let managerChoices = results.map( e => {
+            return {
+                name: e.first_name + " " + e.last_name,
+                value: e.id
             }
-        ]).then(function(res) {
-            connection.query('INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)', [res.firstName, res.lastName, res.roleId, res.managerId], function(err, data) {
+        });
+        managerChoices.push({
+            name: "no manager",value: null
+        })
+        inquirer.prompt([{
+            type: "input",
+            name: "firstName",
+            message: "What is the employees first name?"
+        },
+        {
+            type: "input",
+            name: "lastName",
+            message: "What is the employees last name?"
+        },
+        {
+            type: "number",
+            name: "roleId",
+            message: "What is the employees role ID"
+        },
+        {
+            type: "list",
+            name: "managerId",
+            message: "What is the employees manager's ID?",
+            choices: managerChoices
+        }
+        ]).then(function (res) {
+            connection.query('INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)', [res.firstName, res.lastName, res.roleId, res.managerId], function (err, data) {
                 if (err) throw err;
                 console.table("Successfully Inserted");
                 startQuestions();
             })
         })
-    }
+    });
+
+
+}
